228a9e10bfda92d3113a2c8c2ef4e1596474a724
Federico Cerutti <federico@ceres-c.it>
2019-03-02 17:35:46 +0100

    Definitive SQL setup script for version 1 and renamed Main class

--- 4events/db/four_events_db_dump.sql
+++ 4events/db/four_events_db_dump.sql
@@ -28,12 +28,28 @@ CREATE TABLE public.default_event (
     end_time time without time zone,
     notes text,
     CONSTRAINT default_event_cost_check CHECK ((cost >= 0.0)),
-    CONSTRAINT default_event_partecipants_num_check CHECK ((partecipants_num > 0))
+    CONSTRAINT default_event_partecipants_num_check CHECK ((partecipants_num > 0)),
+    CONSTRAINT default_event_pkey PRIMARY KEY (event_id)
 );
 
 --
--- Name: default_event default_event_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: soccer_game; Type: TABLE; Schema: public; Owner: postgres; Inherits: default_event
 --
 
-ALTER TABLE ONLY public.default_event
-    ADD CONSTRAINT default_event_pkey PRIMARY KEY (event_id);
+CREATE TABLE public.soccer_game (
+    gender character(1) NOT NULL,
+    age_min smallint NOT NULL,
+    age_max smallint NOT NULL
+)
+INHERITS (public.default_event);
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.categories (
+	event_type text,
+	name text,
+	descr text,
+	CONSTRAINT categories_pkey PRIMARY KEY (event_type)
+);
